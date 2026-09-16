3cce528a62ae22a221057d77e5b19252d46057f7
Vorn Sivfong <fongmac@Vorns-MacBook-Pro.local>
2026-09-16 14:56:50 +0700

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.route.js';
+import messagesRoutes from './routes/message.route.js';
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+app.use("/api/auth/", authRoutes);
+app.use("/api/message/", messagesRoutes);
+
+
+app.listen(PORT,() => {
+  console.log('Server is running on port: ' + PORT);
+});
